5cddf5a78ee1e20b105a335753de6e816bfc2e4a
chanaphons-maker-CEO <chanaphon.s@kkumail.com>
2025-08-15 10:35:15 +0700

    Create db.js

---
+++ connectable/server/src/db.js
@@ -0,0 +1,16 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+export async function query(q, params) {
+  const client = await pool.connect();
+  try {
+    const res = await client.query(q, params);
+    return res;
+  } finally {
+    client.release();
+  }
+}
